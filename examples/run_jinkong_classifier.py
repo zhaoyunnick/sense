1e893638b2e33674eb0573281bcecbf6d59744a3
Zhaoyun Zhang <u7545864@anu.edu.au>
2023-02-08 09:23:44 +0800

    update resource file and classifier

--- examples/run_jinkong_classifier.py
+++ examples/run_jinkong_classifier.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 """
+Real time detection of 6 hand gesture events. Compared to `run_action_recognition`, the models used
+in this script were trained to trigger the correct class only for a short period of time right after
+the hand gesture occurred. This behavior policy makes it easier to quickly trigger multiple hand
+gestures in a row.
+
 Usage:
   run_gesture_control.py [--camera_id=CAMERA_ID]
                          [--path_in=FILENAME]
@@ -18,23 +23,26 @@ Options:
   --model_version=VERSION    Version of the model to be used.
   --use_gpu                  Whether to run inference on the GPU or not.
 """
+from typing import Callable
+from typing import Optional
 
-import json
-import os.path
 from docopt import docopt
 
 import sense.display
-from sense.loading import load_weights_from_resources
 from sense.controller import Controller
 from sense.downstream_tasks.nn_utils import LogisticRegression
 from sense.downstream_tasks.nn_utils import Pipe
-from sense.downstream_tasks.postprocess import PostprocessClassificationOutput
 from sense.downstream_tasks.postprocess import AggregatedPostProcessors
-from sense.loading import build_backbone_network
-from sense.loading import update_backbone_weights
 from sense.downstream_tasks.postprocess import EventCounter
+from sense.downstream_tasks.postprocess import PostprocessClassificationOutput
+from sense.loading import get_relevant_weights
+from sense.loading import build_backbone_network
 from sense.loading import ModelConfig
 
+
+SUPPORTED_MODEL_CONFIGURATIONS = [
+    ModelConfig('StridedInflatedEfficientNet', 'pro', ['jk_gesture_control']),
+]
 
 LABEL2INT = {
     "rotation-ccw": 0,
@@ -48,7 +56,7 @@ LABEL2INT = {
     "swipe-up-inv": 8,
     "zoom": 9
 }
-INT2LAB = {value: key for key, value in LABEL2INT.items()}
+INT2LABEL = {value: key for key, value in LABEL2INT.items()}
 
 ENABLE_LAB = [
     "rotation-ccw",
@@ -62,49 +70,45 @@ ENABLE_LAB = [
     "swipe-up-inv",
     "zoom"
 ]
-LAB_THRESHOLDS = {key: 0.6 if key in ENABLE_LAB else 1. for key in LABEL2INT}
+LAB_THRESHOLDS = {key: 0.8 if key in ENABLE_LAB else 1. for key in LABEL2INT}
 
+def run_jinkong(model_name: str,
+                        model_version: str,
+                        title: Optional[str] = None,
+                        display_fn: Optional[Callable] = None,
+                        **kwargs):
+    """
+    :param model_name:
+        Model from backbone (StridedInflatedEfficientNet or StridedInflatedMobileNetV2).
+    :param model_version:
+        Model version (pro or lite)
+    :param title:
+        Title of the image frame on display.
+    :param display_fn:
+        Optional function to further process displayed image
+    """
+    # Load weights
+    selected_config, weights = get_relevant_weights(
+        SUPPORTED_MODEL_CONFIGURATIONS,
+        model_name,
+        model_version
+    )
 
-if __name__ == "__main__":
-    # Parse arguments
-    args = docopt(__doc__)
+    # Load backbone network
+    backbone_network = build_backbone_network(selected_config, weights['backbone'],
+                                              weights_finetuned=weights['jk_gesture_control'])
 
-    camera_id = int(args['--camera_id'] or 0)
-    path_in = args['--path_in'] or None
-    path_out = args['--path_out'] or None
-    title = args['--title'] or None
-    use_gpu = args['--use_gpu']
-
-    # load backbone network
-    backbone_model_config = ModelConfig('StridedInflatedEfficientNet', 'pro', [])
-    backbone_model_weight = backbone_model_config.load_weights()['backbone']
-
-    # load custom classifier
-    classifier = load_weights_from_resources('jk_gesture_control/best_classifier.checkpoint')
-
-    # update original weight in case some intermittent layers have been finned
-    backbone_network = build_backbone_network(backbone_model_config, backbone_model_weight,
-                                              weights_finetuned=classifier)
-
-    # Create backbone network
-    backbone_network = build_backbone_network(backbone_model_config, backbone_model_weight)
-
-    # load label file
-    '''
-    with open(os.path.join(classiffier, 'label2int.json')) as file:
-        class2label = json.load(file)
-    INT2LABEL = {value: key for key, value in class2label.item()}
-    '''
+    # Create a logistic regression classifier
     gesture_classifier = LogisticRegression(num_in=backbone_network.feature_dim,
-                                            num_out=len(INT2LAB))
-    gesture_classifier.load_state_dict(classifier)
+                                            num_out=len(INT2LABEL))
+    gesture_classifier.load_state_dict(weights['jk_gesture_control'])
     gesture_classifier.eval()
 
     # Concatenate backbone network and logistic regression
     net = Pipe(backbone_network, gesture_classifier)
 
     postprocessor = [
-        PostprocessClassificationOutput(INT2LAB, smoothing=4),
+        PostprocessClassificationOutput(INT2LABEL, smoothing=1),
         AggregatedPostProcessors(
             post_processors=[
                 EventCounter(key, LABEL2INT[key], LAB_THRESHOLDS[key]) for key in ENABLE_LAB
@@ -133,15 +137,7 @@ if __name__ == "__main__":
                                                    display_ops=display_ops,
                                                    border_size_top=border_size_top,
                                                    border_size_right=border_size_right,
-                                                   )
-    '''
-    display_ops = [
-        sense.display.DisplayFPS(expected_camera_fps=net.fps,
-                                 expected_inference_fps=net.fps / net.step_size),
-        sense.display.DisplayTopKClassificationOutputs(top_k=1, threshold=0.1),
-    ]
-    display_results = sense.display.DisplayResults(title=title, display_ops=display_ops)
-    '''
+                                                   display_fn=display_fn)
 
     # Run live inference
     controller = Controller(
@@ -149,15 +145,21 @@ if __name__ == "__main__":
         post_processors=postprocessor,
         results_display=display_results,
         callbacks=[],
-        camera_id=camera_id,
-        path_in=path_in,
-        path_out=path_out,
-        use_gpu=use_gpu
+        **kwargs
     )
     controller.run_inference()
 
 
+if __name__ == "__main__":
+    # Parse arguments
+    args = docopt(__doc__)
 
-
-
-
+    run_jinkong(
+        camera_id=int(args['--camera_id'] or 0),
+        path_in=args['--path_in'] or None,
+        path_out=args['--path_out'] or None,
+        title=args['--title'] or None,
+        model_name=args['--model_name'] or None,
+        model_version=args['--model_version'] or None,
+        use_gpu=args['--use_gpu'],
+    )
